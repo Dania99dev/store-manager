74c372e6ea007d3d1a71d39da36fe4a62075ccf3
Dania99dev <dania99.dev@gmail.com>
2021-03-05 12:11:51 +0330

    HTML markup created

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,5 +1,30 @@
 <template>
-  <h1>Products page</h1>
+  <div class="products">
+    <h1>Products</h1>
+    <form>
+      <input type="search" name="search" />
+      <button type="submit"><i class="fas fa-search"></i></button>
+    </form>
+    <button><i class="fas fa-plus"></i> Add product</button>
+    <table>
+      <tr>
+        <th>#</th>
+        <th>Product</th>
+        <th>No.</th>
+        <th>Buy price</th>
+        <th>Sell price</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>Michael Kors Men's Slim Runway Stainless Steel Quartz Watch</td>
+        <td>5</td>
+        <td>89</td>
+        <td>118.94</td>
+      </tr>
+    </table>
+
+    <p>54 products</p>
+  </div>
 </template>
 
 <script lang="ts">
