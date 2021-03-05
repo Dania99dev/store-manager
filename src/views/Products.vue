286967b07032870187aa387c8760ae76fcdd8a7c
Dania99dev <dania99.dev@gmail.com>
2021-03-05 11:42:54 +0330

    Products page set up

---
+++ src/views/Products.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Products page</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "Products"
+});
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/_variables.scss";
+</style>
