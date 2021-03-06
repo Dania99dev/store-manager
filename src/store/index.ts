ddd1f06d1fb49a2b5e3d0217ef52d0e9831f37f7
Dania99dev <dania99.dev@gmail.com>
2021-03-06 11:50:16 +0330

    products state and action added

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,18 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    products: []
+  },
+  mutations: {
+    setProducts: (state, products) => state.products = products
+  },
+  actions: {
+    async fetchProducts({ commit }) {
+      const res = await fetch("http://localhost:5000/products");
+      const data = await res.json();
+      commit('setProducts', data);
+    }
+  },
   modules: {}
 });
